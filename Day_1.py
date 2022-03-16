d9898a6443c7e284a3460d55454de9fd3641cbac
Gagan Rasineni <67116251+GagsR99@users.noreply.github.com>
2022-03-16 14:13:06 +0530

    Add files via upload

---
+++ Day_1.py
@@ -0,0 +1,20 @@
+"""  INTRODUCTION  """
+
+# # # # # print("Hello world")
+# # # # greetings = "Hi!"
+# # # # name = "Gagan"
+# # # # con = "I am glad to meet you."
+
+# # # # # print(greetings, end="")
+# # # # # print(name, end="")
+# # # # # print(con)
+
+# # # # print(greetings, name, con)
+
+# # # num = int(input("Enter a number: "))
+# # # print(num)
+# # # print(type(num))
+
+# num = 14%30
+# print(num)
+
---
+++ Day_2.py
@@ -0,0 +1,114 @@
+"""  LOOPS  """
+
+# for
+# while
+# nested for
+
+# a = input("Enter string here: ")
+# print(len(a))
+
+# for x in "banana" :
+#     print(x)
+
+# for i in range(-18,-10):
+#     print(i)
+
+# for i in range(0,10,2):   # (start, stop, increment/decrement)
+#     print(i)
+
+# sum = 0
+# n = int(input())
+# for i in range(n+1):
+#     # sum = sum + i
+#     sum += i
+
+# print(sum)
+
+# sum = 0
+# n = int(input())
+# i = 0
+# while(i<=n):
+#     sum+=i
+#     i+=1
+
+# print(sum)
+
+# sum = 0
+# n = int(input())
+# for i in range(1,n):
+#     cube = i**3
+#     sum+=cube
+
+# print(sum)
+
+# sum = 0
+# n = int(input())
+# i = 0
+# while(i<=n):
+#     cube = i**3
+#     sum+=cube
+#     i+=1
+
+# print(sum)
+
+""" LOOP CONTROL STATEMENTS """
+# continue
+# break
+
+# for i in range(1,10):
+#     if(i==5):
+#         break
+#     print(i)
+
+# i = 0
+# while(i<=10):
+#     if(i==5):
+#         break
+#     i+=1
+#     print(i)
+
+# for i in range(1,6):
+#     if(i==3):
+#         continue
+#     print(i)
+
+# i = 0
+# while(i<6):
+#     i+=1
+#     if(i==3):
+#         continue
+#     print(i)
+
+# i = 0
+# while(i<10):
+#     i+=1
+#     if(i==5 and i==7):
+#         continue
+#     print(i)
+
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(i+j, end=" ")
+#     print("\n")
+
+# i = 0
+# j = 1
+# while(i<5):
+#     i+=1
+#     while(j<5):
+#         print(i+j, end=" ")
+#         j+=1
+#     print("\n")
+
+# n = int(input("Enter any number: "))
+# sum = 0
+# for i in range(0,n+1):
+#     sum+=i
+# print("Avg: ", sum/n)
+
+# n = int(input("Enter number here: "))
+# sum = 0
+# for i in range(0,n+1):          # SUM OF SQUARES
+#     sum+=(i**2)
+# print(sum)
+
