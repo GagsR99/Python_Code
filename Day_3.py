84c487ae9554b4550619e88fb42818f7033464a8
Gagan R <rasinenigagan@gmail.com>
2022-03-21 18:44:42 +0530

    New Upload

--- Day_3.py
+++ Day_3.py
@@ -1,1 +1,15 @@
-print("Hello")
+# GIT Commands 
+git init
+git remote add origin 
+git add 
+git commit -m "message"
+git push origin master 
+
+#LISTS
+# ls = ["Gagan", "Kriti", "Akash", ...... "Vaishnavi"] # How to declare a list.
+
+# ls = ["name", True, 44.2, 57]
+# print(ls) 
+# print(type(ls))
+
+ 
