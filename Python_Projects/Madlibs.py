467917c335769b52795d9a48e773c51be938a000
Gagan R <rasinenigagan@gmail.com>
2022-05-02 19:39:56 +0530

    Madlib Project

---
+++ Python_Projects/Madlibs.py
@@ -0,0 +1,16 @@
+# # String Concatenation
+# youtuber = "Gagan Rasineni"
+
+# print("My name is " + youtuber)             # My name is Gagan Rasineni
+# print("My name is {}".format(youtuber))     # My name is Gagan Rasineni
+# print(f"My name is {youtuber}")             # My name is Gagan Rasineni
+
+# adj = input("Adjective: ")
+# verb1 = input("Verb: ")
+# verb2 = input("Verb: ")
+# famous_person = input("Famous Person: ")
+
+# madlib = f"Computer programming is so {adj}! It makes me so excited all the time beacuse \
+#     I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}."
+
+# print(madlib)
